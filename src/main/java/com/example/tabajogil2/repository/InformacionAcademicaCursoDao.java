bdafc6649efeb74202c3260bf372831198206716
JuaneSsj <juan_marin23232@elpoli.edu.co>
2025-06-19 00:14:45 -0500

    un cambio

--- src/main/java/com/example/tabajogil2/repository/InformacionAcademicaCursoDao.java
+++ src/main/java/com/example/tabajogil2/repository/InformacionAcademicaCursoDao.java
@@ -13,20 +13,20 @@ public class InformacionAcademicaCursoDao {
     }
 
     public boolean yaExisteRelacion(Long idCurso, Long idInformacion) {
-        String sql = "SELECT COUNT(*) FROM informacion_academica_curso WHERE id_curso = ? AND id_informacion = ?";
+        String sql = "SELECT COUNT(*) FROM informacionacademicacurso WHERE idCurso = ? AND idInformacion = ?";
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class, idCurso, idInformacion);
         return count != null && count > 0;
     }
 
     public void guardarRelacion(Long idCurso, Long idInformacion) {
         if (!yaExisteRelacion(idCurso, idInformacion)) {
-            String sql = "INSERT INTO informacion_academica_curso (id_curso, id_informacion) VALUES (?, ?)";
+            String sql = "INSERT INTO informacionacademicacurso (idCurso, idInformacion) VALUES (?, ?)";
             jdbcTemplate.update(sql, idCurso, idInformacion);
         }
     }
 
     public void eliminarRelacionesDeCurso(Long idCurso) {
-        String sql = "DELETE FROM informacion_academica_curso WHERE id_curso = ?";
+        String sql = "DELETE FROM informacionacademicacurso WHERE idCurso = ?";
         jdbcTemplate.update(sql, idCurso);
     }
 }
